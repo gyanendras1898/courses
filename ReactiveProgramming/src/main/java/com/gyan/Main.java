c488395c1cc9c782f5e44797d1e6a9c68f26ae7a
gyanendra <gyanendras1898@gmail.com>
2024-07-11 02:51:15 +0530

    initial commit: pom and logger setting

---
+++ ReactiveProgramming/src/main/java/com/gyan/Main.java
@@ -0,0 +1,10 @@
+package com.gyan;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
